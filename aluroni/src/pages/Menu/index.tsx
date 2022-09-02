20628c34349fe5cb79a15bf1a352c4480cdd327a
Hugo Deiró <hugodeiro@gmail.com>
2022-09-02 10:26:46 -0300

    Import SVG as an SVG item

--- aluroni/src/pages/Menu/index.tsx
+++ aluroni/src/pages/Menu/index.tsx
@@ -1,10 +1,12 @@
 import style from './Menu.module.scss';
+// Import SVG as an SVG tag
+import { ReactComponent as Logo} from '../../assets/img/logo.svg';
 
 export default function Menu() {
   return (
     <main>
       <nav className={style["Menu"]}>
-        Menu!
+        <Logo />
       </nav>
     </main>
   );
