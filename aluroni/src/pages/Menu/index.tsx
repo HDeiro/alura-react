f6a92e61708fdb3b80ef7cacf07118dbf4a360f4
Hugo Deiró <hugodeiro@gmail.com>
2022-09-05 10:51:28 -0300

    Using normalize.css instead of meyer css reset & add font family

--- aluroni/src/pages/Menu/index.tsx
+++ aluroni/src/pages/Menu/index.tsx
@@ -1,4 +1,8 @@
 import style from './Menu.module.scss';
+
+// Import normalize.css installed via npm
+import 'normalize.css';
+
 // Import SVG as an SVG tag
 import { ReactComponent as Logo} from 'assets/img/logo.svg';
 
