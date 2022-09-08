e0c11351c2252769214ef48595c7e64524cb7f9e
Hugo Deiró <hugodeiro@gmail.com>
2022-09-08 13:08:24 -0300

    Create base of not found page

---
+++ aluroni/src/pages/NotFound/index.tsx
@@ -0,0 +1,11 @@
+import theme from 'styles/theme.module.scss';
+import styles from './not-found.module.scss';
+
+export default function NotFound() {
+  return (
+    <section>
+      not found
+    </section>
+  );
+}
+  
