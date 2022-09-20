b4f376193e0bd09ac88fa1fa333a7ddcae2617a0
Hugo Deiró <hugodeiro@gmail.com>
2022-09-20 15:36:08 -0300

    Deleting & refreshing list

--- alfood/client/src/paginas/Admin/AdminRestaurantes/index.tsx
+++ alfood/client/src/paginas/Admin/AdminRestaurantes/index.tsx
@@ -1,4 +1,4 @@
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+import { Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react"
 import { Link } from "react-router-dom";
@@ -8,10 +8,23 @@ export default function AdminRestaurantes() {
     const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([]);
 
     useEffect(() => {
+        list();
+    }, []);
+
+    const list = () => {
         axios.get<IRestaurante[]>('http://localhost:8000/api/v2/restaurantes/')
             .then(({ data }) => setRestaurantes(data))
             .catch(console.error);
-    }, []);
+    }
+
+    const deleteRestaurant = (restaurante: IRestaurante) => {
+        axios.delete(`http://localhost:8000/api/v2/restaurantes/${restaurante.id}/`)
+            .then(() => {
+                console.log('Deletado');
+                list();
+            })
+            .catch(console.error);
+    }
 
     return (
         <TableContainer component={Paper}>
@@ -20,6 +33,7 @@ export default function AdminRestaurantes() {
                     <TableRow>
                         <TableCell>Name</TableCell>
                         <TableCell>Editar</TableCell>
+                        <TableCell>Deletar</TableCell>
                     </TableRow>
                 </TableHead>
 
@@ -29,6 +43,14 @@ export default function AdminRestaurantes() {
                             <TableCell>{restaurante.nome}</TableCell>
                             <TableCell>
                                 <Link to={`/admin/restaurantes/${restaurante.id}`}>Editar</Link>
+                            </TableCell>
+                            <TableCell>
+                                <Button 
+                                    color="error"
+                                    variant="outlined"
+                                    onClick={() => deleteRestaurant(restaurante)}>
+                                    Excluir
+                                </Button>
                             </TableCell>
                         </TableRow>
                     )}
