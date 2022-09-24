c8595e941367f0b9d276dedbd9e751681b11d8ed
Hugo Deiró <hugodeiro@gmail.com>
2022-09-24 08:59:41 -0300

    Update on dragfinish

--- event-tracker/src/components/Calendario/index.tsx
+++ event-tracker/src/components/Calendario/index.tsx
@@ -2,9 +2,9 @@
 import React from 'react'
 import style from './Calendario.module.scss';
 import ptBR from './localizacao/ptBR.json'
-import Kalend, { CalendarView } from 'kalend'
+import Kalend, { CalendarEvent, CalendarView, OnEventDragFinish } from 'kalend'
 import 'kalend/dist/styles/index.css';
-import { useRecoilValue } from 'recoil';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { listaDeEventosState } from '../../state/atom';
 
 interface IKalendEvento {
@@ -16,7 +16,9 @@ interface IKalendEvento {
 }
 
 const Calendario: React.FC = () => {
+  const setListaEventos = useSetRecoilState(listaDeEventosState);
   const eventos = useRecoilValue(listaDeEventosState);
+
   const eventosKalend = new Map<string, IKalendEvento[]>();
 
   eventos.forEach(evento => {
@@ -33,9 +35,26 @@ const Calendario: React.FC = () => {
     })
   })
 
+  const onEventDragFinish: OnEventDragFinish = (_, updatedEvent: CalendarEvent) => {
+    const evt = eventos.find(evento => evento.id === updatedEvent.id);
+
+    if (!evt) return;
+    
+    const newEvt = {
+      ...evt,
+      inicio: new Date(updatedEvent.startAt),
+      fim: new Date(updatedEvent.endAt),
+    };
+
+    setListaEventos(evts => [...evts.map(evtItem => 
+      evtItem.id === newEvt.id ? newEvt : evtItem
+    )]);
+  }
+
   return (
     <div className={style.Container}>
       <Kalend
+        onEventDragFinish={onEventDragFinish}
         events={Object.fromEntries(eventosKalend)}
         initialDate={new Date().toISOString()}
         hourHeight={60}
--- event-tracker/src/state/atom.ts
+++ event-tracker/src/state/atom.ts
@@ -6,7 +6,7 @@ export const listaDeEventosState = atom<IEvento[]>({
     default: [{
         "descricao": "Estudar React",
         "inicio": new Date("2022-09-24T09:00"),
-        "fim": new Date("2022-09-25T13:00"),
+        "fim": new Date("2022-09-24T10:00"),
         "completo": false,
         "id": 1642342747
     },
